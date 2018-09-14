c371d573c42fe86ffc979b0ea5036f27a88ff496
tcp <283922842@qq.com>
2018-09-14 17:33:45 +0800

    修复日志sdk初始化失败 初始化延迟导致的日志未上传问题

--- app/src/main/java/tv/newtv/cboxtv/cms/util/LogUploadUtils.java
+++ app/src/main/java/tv/newtv/cboxtv/cms/util/LogUploadUtils.java
@@ -3,13 +3,11 @@ package tv.newtv.cboxtv.cms.util;
 import android.annotation.SuppressLint;
 import android.util.Log;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import tv.icntv.logsdk.logSDK;
 import tv.newtv.cboxtv.Constant;
 
@@ -18,40 +16,49 @@ public class LogUploadUtils {
 
     private static boolean isUpload = true;//日志初始化和上传的开关
     private static logSDK logUpload;//日志上传对象
-    private static boolean isToInit = true;
-    private static boolean isinit = false;
+    //初始化是否成功
+    private volatile static boolean isInit = false;
+    //是否正在初始化
+    private volatile static boolean isInitializing = false;
+
+    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private static List<Runnable> runnableList = new ArrayList<>();
+    private static int retryNumber = 0;
 
     @SuppressLint("CheckResult")
-    public static boolean initSDk() {
-
-        if (isinit) {
-            return isinit;
+    public static void initSDk() {
+        if (isInit || isInitializing) {
+            return ;
         }
+        isInitializing = true;
 
-        //日志上传sdk初始化
-        Observable.create(new ObservableOnSubscribe<Boolean>() {
+        executorService.execute(new Runnable() {
             @Override
-            public void subscribe(ObservableEmitter<Boolean> e) throws Exception {
-                if(isinit){
-                    e.onNext(true);
-                    return;
-                }
+            public void run() {
                 logUpload = logSDK.getInstance();
-                Boolean b = logUpload.sdkInit(Constant.LOG_ADDR, "", Constant.UUID, Constant
+                isInit = logUpload.sdkInit(Constant.LOG_ADDR, "", Constant.UUID, Constant
                         .CHANNEL_CODE, Constant.APPKEY);
-                e.onNext(b);
-            }
-        }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Boolean>() {
-                    @Override
-                    public void accept(Boolean value) throws Exception {
-                        isinit = value;
-                        Log.i(TAG, "logsdk初始化结果：" + value);
-                    }
-                });
 
-        return isinit;
+                if(isInit){
+                    isInitializing = false;
+                    for(Runnable runnable : runnableList){
+                        executorService.submit(runnable);
+                    }
+                    runnableList.clear();
+                } else {
+                    try {
+                        if(retryNumber++ < 10){
+                            Thread.sleep(10 * 1000);
+                            executorService.submit(this);
+                        }else {
+                            isInitializing = false;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     /**
@@ -61,33 +68,26 @@ public class LogUploadUtils {
      */
     @SuppressLint("CheckResult")
     public static void uploadLog(final int type, final String content) {
-        int result = -1;
-        RxBus.get().post(Constant.INIT_SDK, Constant.INIT_LOGSDK);
         if (!isUpload) {
             return;
         }
-        if (null != logUpload) {
 
-            //日志上传sdk初始化
-            Observable.create(new ObservableOnSubscribe<Integer>() {
-                @Override
-                public void subscribe(ObservableEmitter<Integer> e) throws Exception {
-                    Log.e("logsdk", "日志报上内容=" + content + ",type=" + type);
-                    e.onNext(logUpload.logUpload(type, content));
-                }
-            }).subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new Consumer<Integer>() {
-                        @Override
-                        public void accept(Integer value) throws Exception {
-                            Log.e(TAG, "日志上传结果result=：" + value);
-                        }
-                    });
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "日志上报内容=" + content + ",type=" + type
+                        +",日志上传结果result="+logUpload.logUpload(type,content));
+            }
+        };
 
+        if(isInit){
+            executorService.execute(runnable);
+        }else {
+            runnableList.add(runnable);
+            if(!isInitializing){
+                initSDk();
+            }
         }
     }
 
-    public static boolean isInit() {
-        return isinit;
-    }
 }
