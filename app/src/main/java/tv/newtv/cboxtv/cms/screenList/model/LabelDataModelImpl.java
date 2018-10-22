0dd9562a1fb61992fee7ee670af9bbe8f3703a01
123456789kai <734871387@qq.com>
2018-10-22 17:54:10 +0800

    优化代码

--- app/src/main/java/tv/newtv/cboxtv/cms/screenList/model/LabelDataModelImpl.java
+++ app/src/main/java/tv/newtv/cboxtv/cms/screenList/model/LabelDataModelImpl.java
@@ -6,18 +6,12 @@ import android.util.Log;
 import com.google.gson.Gson;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import tv.newtv.cboxtv.cms.screenList.api.LabelApi;
 import tv.newtv.cboxtv.cms.screenList.bean.LabelDataBean;
 import tv.newtv.cboxtv.cms.screenList.common.Common;
