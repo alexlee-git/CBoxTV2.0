a43e1fbacd1d098c4dd8991b00e0eaa85ca522dd
lixin <li.xin2@icntv.tv>
2018-11-21 13:54:22 +0800

    设置用户中心数据列表中如果评分字段无信息或是没有评分显示需求时,不显示评分控件

--- app/src/main/java/tv/newtv/cboxtv/uc/v2/sub/UserCenterUniversalAdapter.java
+++ app/src/main/java/tv/newtv/cboxtv/uc/v2/sub/UserCenterUniversalAdapter.java
@@ -66,6 +66,11 @@ public class UserCenterUniversalAdapter extends RecyclerView.Adapter<UserCenterU
             String score = info.getGrade();
             if (!TextUtils.isEmpty(score) && !TextUtils.equals("null", score)) {
                 holder.score.setText(score);
+                holder.score.setVisibility(View.VISIBLE);
+                Log.d("pf", "vv : " + score);
+            } else {
+                holder.score.setVisibility(View.INVISIBLE);
+                Log.d("pf", "haha");
             }
 
             holder.subTitle.setText(UserCenterRecordManager.getInstance().getWatchProgress(info.getPlayPosition(), info.getDuration()));
@@ -84,8 +89,12 @@ public class UserCenterUniversalAdapter extends RecyclerView.Adapter<UserCenterU
             // 角标 & 主标 & 海报 & 更新剧集 & 评分
             String score = info.getGrade();
             if (!TextUtils.isEmpty(score) && !TextUtils.equals(score, "null")) {
+                holder.score.setVisibility(View.VISIBLE);
                 holder.score.setText(score);
+            } else {
+               holder.score.setVisibility(View.INVISIBLE);
             }
+
             holder.episode.setText(getEpisode(info));
             holder.mask.setVisibility(View.VISIBLE);
             if (holder.superscript != null) {
@@ -100,6 +109,7 @@ public class UserCenterUniversalAdapter extends RecyclerView.Adapter<UserCenterU
         } else if (TextUtils.equals(mContentType, Constant.UC_FOLLOW)) {
             // 主标 & 海报 这两个下面统一做,这里只需将衬托剧集信息和评分区域的蒙版隐藏掉
             holder.mask.setVisibility(View.GONE);
+            holder.score.setVisibility(View.GONE);
         }
 
         // 主标题
