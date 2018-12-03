a13d644fcd9a2f89dbb85b4118ebaf6174a1793b
tcp <283922842@qq.com>
2018-12-03 16:53:31 +0800

    导航栏中点计算错误

--- app/src/main/java/tv/newtv/cboxtv/cms/mainPage/LooperUtil.java
+++ app/src/main/java/tv/newtv/cboxtv/cms/mainPage/LooperUtil.java
@@ -12,7 +12,12 @@ public final class LooperUtil {
     public static final int MAX_VALUE = 2000;
 
     public static int getMiddleValue(int size){
-        int MaxValue = MAX_VALUE - (MAX_VALUE % size);
-        return  (MaxValue - (MaxValue % 2 != 0 ? size : 0)) / 2;
+        int maxValue = MAX_VALUE - (MAX_VALUE % size);
+
+//        return  (maxValue - (maxValue % 2 != 0 ? size : 0)) / 2;
+        if(size == 0){
+            return MAX_VALUE /2;
+        }
+        return maxValue / size / 2 * size;
     }
 }
