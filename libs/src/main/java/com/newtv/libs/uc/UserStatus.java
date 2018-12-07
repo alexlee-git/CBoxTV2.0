66105b0c4682cc195fcb548e0b7f062b08611408
tcp <283922842@qq.com>
2018-12-07 09:58:49 +0800

    登录后保存userid

--- libs/src/main/java/com/newtv/libs/uc/UserStatus.java
+++ libs/src/main/java/com/newtv/libs/uc/UserStatus.java
@@ -2,12 +2,16 @@ package com.newtv.libs.uc;
 
 import android.text.TextUtils;
 
+import com.newtv.libs.Libs;
+import com.newtv.libs.util.SharePreferenceUtils;
+
 public class UserStatus {
     public static final String SIGN_MEMBER_OPEN_NOT = "member_open_not";//未开通会员
     public static final String SIGN_MEMBER_OPEN_CLOSE = "member_open_lose";//已开通，但失效
     public static final String SIGN_MEMBER_OPEN_GOOD = "member_open_good";//已开通，有效
     private static boolean isLogin = false;
     private static String memberSatus;
+    private static String userId;
 
     public static boolean isLogin() {
         return isLogin;
@@ -15,6 +19,15 @@ public class UserStatus {
 
     public static void setIsLogin(boolean isLogin) {
         UserStatus.isLogin = isLogin;
+        if(isLogin){
+            userId = SharePreferenceUtils.getUserId(Libs.get().getContext());
+        } else {
+            userId = "";
+        }
+    }
+
+    public static String getUserId() {
+        return userId;
     }
 
     public static String getMemberSatus() {
