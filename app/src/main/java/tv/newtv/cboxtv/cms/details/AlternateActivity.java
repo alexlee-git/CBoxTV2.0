84dac036bcf9c90b53b46b19493c718909b88dc6
tcp <283922842@qq.com>
2018-12-21 15:26:55 +0800

    【日志】轮播节目，无历史记录上报

--- app/src/main/java/tv/newtv/cboxtv/cms/details/AlternateActivity.java
+++ app/src/main/java/tv/newtv/cboxtv/cms/details/AlternateActivity.java
@@ -108,6 +108,7 @@ public class AlternateActivity extends DetailPageActivity implements
             return;
         }
         LogUploadUtils.uploadLog(Constant.LOG_NODE_DETAIL, "0," + contentUUID);
+        LogUploadUtils.uploadLog(Constant.LOG_NODE_HISTORY, "0," + contentUUID);
         setUp();
     }
 
