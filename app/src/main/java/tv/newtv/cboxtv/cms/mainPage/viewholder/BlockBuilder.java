d8f1785859761c4213c2f6a6e153742d84201002
linzhiying <lin.zhiying@chinaott.onaliyun.com>
2018-12-25 15:08:58 +0800

    增加l_uuid字段到详情页面

--- app/src/main/java/tv/newtv/cboxtv/cms/mainPage/viewholder/BlockBuilder.java
+++ app/src/main/java/tv/newtv/cboxtv/cms/mainPage/viewholder/BlockBuilder.java
@@ -541,7 +541,7 @@ public class BlockBuilder extends BaseBlockBuilder {
         }
         if (info instanceof Program) {
             Program pInfo = (Program) info;
-            JumpUtil.activityJump(mContext, (Program) info);
+            JumpUtil.activityJump(mContext, pInfo);
         } else if (info instanceof Row) {
             Row row = (Row) info;
             JumpUtil.detailsJumpActivity(mContext, row.getContentType(), row.getContentId());
