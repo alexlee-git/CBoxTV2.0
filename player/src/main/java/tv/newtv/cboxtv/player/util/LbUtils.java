112e8fa7a68f706ae789d0472e5f861ffdb86021
tcp <283922842@qq.com>
2018-12-26 14:14:43 +0800

    崩溃处理

--- player/src/main/java/tv/newtv/cboxtv/player/util/LbUtils.java
+++ player/src/main/java/tv/newtv/cboxtv/player/util/LbUtils.java
@@ -1,5 +1,7 @@
 package tv.newtv.cboxtv.player.util;
 
+import android.text.TextUtils;
+
 import com.newtv.libs.util.PlayerTimeUtils;
 
 import java.util.List;
@@ -8,10 +10,19 @@ import tv.newtv.cboxtv.menu.model.Program;
 
 public class LbUtils {
 
-    public static int binarySearch(List<Program> list,int defaultResult) {
+    public static int binarySearch(List<Program> list,int defaultResult){
+        try {
+            int start = 0;
+            if(TextUtils.isEmpty(list.get(0).getStartTime())){
+                start = 1;
+            }
+            return binarySearch(list,start,list.size(),defaultResult);
+        }catch (Exception e){ }
+        return defaultResult;
+    }
+
+    public static int binarySearch(List<Program> list,int start,int end,int defaultResult) {
         long current = System.currentTimeMillis();
-        int start = 0;
-        int end = list.size();
         while ((end - start) > 10) {
             int mid = (end + start) / 2;
             long midValue = parse(list.get(mid).getStartTime());
